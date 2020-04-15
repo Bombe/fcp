e39edac971db853ecbf4df63e30610f2b84dfb58
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-04-15 19:25:59 +0200

    ♻️ Move all argument parsing into a single method

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,37 @@
 use std::error::Error;
 
-use clap::{crate_version, App, Arg, ArgMatches};
-use config::{Config, File};
+use clap::{crate_version, App, Arg};
+use config::File;
 
 use fcp::FcpConnection;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let config = get_config();
-    let default_fcp_hostname = config.get_str("fcp-hostname").unwrap();
-    let arguments = parse_arguments(&default_fcp_hostname);
+    let arguments = parse_arguments();
 
     println!("fcp {}", crate_version!());
 
-    let mut fcp_connection = FcpConnection::default(arguments.value_of("hostname").unwrap());
+    let mut fcp_connection = FcpConnection::default(&arguments.hostname);
     fcp_connection.connect("TestClient")?;
-    println!(
-        "Connected to {}:{}.",
-        arguments.value_of("hostname").unwrap(),
-        9481
-    );
+    println!("Connected to {}:{}.", arguments.hostname, 9481);
 
     Ok(())
 }
 
-fn get_config() -> Config {
+struct FcpArguments {
+    hostname: String,
+}
+
+fn parse_arguments() -> FcpArguments {
     let mut config = config::Config::default();
     config
         .merge(File::with_name("./fcp").required(false))
         .unwrap()
         .set_default("fcp-hostname", "localhost".to_string())
         .unwrap();
-    config
-}
 
-fn parse_arguments(default_fcp_hostname: &str) -> ArgMatches {
-    App::new("fcp")
+    let default_fcp_hostname = config.get_str("fcp-hostname").unwrap();
+
+    let arg_matches = App::new("fcp")
         .version(crate_version!())
         .author("David “Bombe” Roden")
         .about("Command-line FCP client")
@@ -43,7 +40,11 @@ fn parse_arguments(default_fcp_hostname: &str) -> ArgMatches {
                 .short("h")
                 .long("fcp-host")
                 .help("The FCP host name")
-                .default_value(default_fcp_hostname),
+                .default_value(&default_fcp_hostname),
         )
-        .get_matches()
+        .get_matches();
+
+    FcpArguments {
+        hostname: arg_matches.value_of("hostname").unwrap().to_string(),
+    }
 }
