f75954ee2dfda5f1fab5255c96c0142f83096277
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-04-15 19:45:00 +0200

    ✨ Add port number to arguments

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("fcp {}", crate_version!());
 
-    let mut fcp_connection = FcpConnection::default(&arguments.hostname);
+    let mut fcp_connection = FcpConnection::create(&arguments.hostname, arguments.port);
     fcp_connection.connect("TestClient")?;
-    println!("Connected to {}:{}.", arguments.hostname, 9481);
+    println!("Connected to {}:{}.", arguments.hostname, arguments.port);
 
     Ok(())
 }
 
 struct FcpArguments {
     hostname: String,
+    port: u16,
 }
 
 fn parse_arguments() -> FcpArguments {
@@ -27,9 +28,12 @@ fn parse_arguments() -> FcpArguments {
         .merge(File::with_name("./fcp").required(false))
         .unwrap()
         .set_default("fcp-hostname", "localhost".to_string())
+        .unwrap()
+        .set_default("fcp-port", 9481.to_string())
         .unwrap();
 
     let default_fcp_hostname = config.get_str("fcp-hostname").unwrap();
+    let default_fcp_port = config.get_int("fcp-port").unwrap_or(9481).to_string();
 
     let arg_matches = App::new("fcp")
         .version(crate_version!())
@@ -42,9 +46,21 @@ fn parse_arguments() -> FcpArguments {
                 .help("The FCP host name")
                 .default_value(&default_fcp_hostname),
         )
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("fcp-port")
+                .help("The FCP port number")
+                .default_value(&default_fcp_port),
+        )
         .get_matches();
 
     FcpArguments {
         hostname: arg_matches.value_of("hostname").unwrap().to_string(),
+        port: arg_matches
+            .value_of("port")
+            .unwrap()
+            .parse()
+            .unwrap_or(9481),
     }
 }
