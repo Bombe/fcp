ae251556ed9fc2c4a61518c762eef92fdc33e5a7
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-04-15 22:45:12 +0200

    ♻️ Extract configuration for better testability

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::error::Error;
 
-use clap::{crate_version, App, Arg, SubCommand};
+use clap::{crate_version, App, AppSettings, Arg, SubCommand};
 use config::File;
 
-use crate::FcpCommand::Test;
 use fcp::FcpConnection;
 
+use crate::FcpCommand::Test;
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let arguments = parse_arguments(std::env::args().collect());
+    let arguments = parse_arguments(
+        &parse_config_file("./fcp"),
+        std::env::args().skip(1).collect(),
+    );
 
     if arguments.command.is_none() {
         println!("No command to run.");
@@ -37,18 +41,13 @@ enum FcpCommand {
     Test,
 }
 
-fn parse_arguments(args: Vec<String>) -> FcpArguments {
-    let mut config = config::Config::default();
-    config
-        .merge(File::with_name("./fcp").required(false))
-        .unwrap()
-        .set_default("fcp-hostname", "localhost".to_string())
-        .unwrap()
-        .set_default("fcp-port", 9481.to_string())
-        .unwrap();
-
-    let default_fcp_hostname = config.get_str("fcp-hostname").unwrap();
-    let default_fcp_port = config.get_int("fcp-port").unwrap_or(9481).to_string();
+fn parse_arguments(config: &FcpConfig, args: Vec<String>) -> FcpArguments {
+    let default_fcp_hostname = "localhost".to_string();
+    let fcp_hostname = config
+        .fcp_hostname
+        .as_ref()
+        .unwrap_or(&default_fcp_hostname);
+    let fcp_port = config.fcp_port.unwrap_or(9481).to_string();
 
     let arg_matches = App::new("fcp")
         .version(crate_version!())
@@ -60,7 +59,7 @@ fn parse_arguments(args: Vec<String>) -> FcpArguments {
                 .long("fcp-host")
                 .takes_value(true)
                 .help("The FCP host name")
-                .default_value(&default_fcp_hostname),
+                .default_value(&fcp_hostname),
         )
         .arg(
             Arg::with_name("port")
@@ -68,7 +67,7 @@ fn parse_arguments(args: Vec<String>) -> FcpArguments {
                 .long("fcp-port")
                 .takes_value(true)
                 .help("The FCP port number")
-                .default_value(&default_fcp_port),
+                .default_value(&fcp_port),
         )
         .arg(
             Arg::with_name("verbose")
@@ -78,6 +77,7 @@ fn parse_arguments(args: Vec<String>) -> FcpArguments {
                 .help("Be verbose"),
         )
         .subcommand(SubCommand::with_name("test").about("Tests whether a node is reachable"))
+        .setting(AppSettings::NoBinaryName)
         .get_matches_from(args);
 
     FcpArguments {
@@ -92,5 +92,40 @@ fn parse_arguments(args: Vec<String>) -> FcpArguments {
             _ => None,
         },
         verbose: arg_matches.is_present("verbose"),
+    }
+}
+
+fn parse_config_file(config_file: &str) -> FcpConfig {
+    let mut config = config::Config::default();
+    config
+        .merge(File::with_name(config_file).required(false))
+        .unwrap()
+        .set_default("fcp-hostname", "localhost".to_string())
+        .unwrap()
+        .set_default("fcp-port", 9481.to_string())
+        .unwrap();
+
+    FcpConfig {
+        fcp_hostname: config.get_str("fcp-hostname").ok(),
+        fcp_port: config.get_int("fcp-port").map(|p| p as u16).ok(),
+    }
+}
+
+/// Configuration as read from a configuration file.
+#[derive(Debug, PartialEq)]
+struct FcpConfig {
+    /// The hostname to connect to.
+    fcp_hostname: Option<String>,
+
+    /// The port number to connect to.
+    fcp_port: Option<u16>,
+}
+
+impl FcpConfig {
+    fn create(fcp_hostname: Option<String>, fcp_port: Option<u16>) -> FcpConfig {
+        FcpConfig {
+            fcp_hostname,
+            fcp_port,
+        }
     }
 }
