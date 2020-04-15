38eaaa9e52e68fb74e54cdb0920737bfb25cdd45
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-04-15 16:55:54 +0200

    ♻️ Extract config and argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 use std::error::Error;
 
-use clap::{crate_version, App, Arg};
-use config::File;
+use clap::{crate_version, App, Arg, ArgMatches};
+use config::{Config, File};
 
 use fcp::FcpConnection;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut config = config::Config::default();
+    let config = get_config();
+    let default_fcp_hostname = config.get_str("fcp-hostname").unwrap();
+    let arguments = parse_arguments(&default_fcp_hostname);
 
+    println!("fcp {}", crate_version!());
+
+    let mut fcp_connection = FcpConnection::default(arguments.value_of("hostname").unwrap());
+    fcp_connection.connect("TestClient")?;
+    println!(
+        "Connected to {}:{}.",
+        arguments.value_of("hostname").unwrap(),
+        9481
+    );
+
+    Ok(())
+}
+
+fn get_config() -> Config {
+    let mut config = config::Config::default();
     config
         .merge(File::with_name("./fcp").required(false))
         .unwrap()
         .set_default("fcp-hostname", "localhost".to_string())
         .unwrap();
+    config
+}
 
-    let default_fcp_hostname = config.get_str("fcp-hostname").unwrap();
-
-    let matches = App::new("fcp")
+fn parse_arguments(default_fcp_hostname: &str) -> ArgMatches {
+    App::new("fcp")
         .version(crate_version!())
         .author("David “Bombe” Roden")
         .about("Command-line FCP client")
@@ -25,19 +43,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .short("h")
                 .long("fcp-host")
                 .help("The FCP host name")
-                .default_value(default_fcp_hostname.as_str()),
+                .default_value(default_fcp_hostname),
         )
-        .get_matches();
-
-    println!("fcp {}", crate_version!());
-
-    let mut fcp_connection = FcpConnection::default(matches.value_of("hostname").unwrap());
-    fcp_connection.connect("TestClient")?;
-    println!(
-        "Connected to {}:{}.",
-        matches.value_of("hostname").unwrap(),
-        9481
-    );
-
-    Ok(())
+        .get_matches()
 }
